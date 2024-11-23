897c8cba0436185994f496a2620b671435d75695
Orzech <olekorzech557@gmail.com>
2024-11-23 20:05:23 +0100

    Pierwszy commit

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,3 +1,3 @@
 export default function Movie(props: {url: string}) {
-    return <div className="display-video-container"><iframe style={{border: 'none'}} src={props.url} width="1600" height="900" allowFullScreen={true} allowscriptaccess="always" scrolling="no"></iframe></div>
+    return <div className="display-video-container"><iframe style={{border: 'none'}} src={props.url} width="1600" height="900" allowFullScreen={true} scrolling="no"></iframe></div>
 }
